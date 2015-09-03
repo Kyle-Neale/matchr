2459fe07af0e215a6c879c2884bcb42e9b650f03
tyler liu <replicat0r.misc@gmail.com>
2015-09-03 00:03:59 -0400

    added friendship methods in the model to request, accept and remove matches

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: "Friendship" , foreign_key: "friend_id",dependent: :destroy
 
   has_attached_file :avatar,
     :styles => {:medium => "370x370", :thumb => "100x100"},
@@ -15,7 +17,6 @@ class User < ActiveRecord::Base
   end
 
   def self.create_user_from_facebook(auth)
-    byebug
 
     create ( {
                avatar: process_uri(auth['info'][:image] + "?width=999999"),
@@ -28,6 +29,25 @@ class User < ActiveRecord::Base
                location: auth[:info][:location],
                bio: auth[:extra][:raw_info][:bio]
     })
+  end
+
+  def request_match(user2)
+    self.friendships.create(friend: user2)
+
+  end
+
+  def accept_match(user2)
+    self.friendships.where(friend: user2).first.update_attribute(:state, "ACTIVE")
+  end
+
+  def remove_match(user2)
+    inverse_friendship = inverse_friendships.where(user_id: user2).first
+
+    if inverse_friendship
+      self.inverse_friendships.where(user_id: user2).first.destroy
+    else
+      self.friendships.where(friend_id: user2).first.destroy
+    end
   end
 
   private
