b533fb4bb8550db599384658008a7854cb0f914e
tyler liu <replicat0r.misc@gmail.com>
2015-09-03 13:57:41 -0400

    added dynamic view helper for user profile views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def name(user)
+    if user.name
+
+      case user
+      when user
+        user.name.partition(' ').first
+      when @user
+        user.name.partition(' ').first
+      end
+    end
+  end
+
+  def age(user)
+    if user.date_of_birth
+      user.date_of_birth + " yrs +"
+    end
+  end
+
+  def location(user)
+    if user.location
+      case user
+      when user
+        user.location.partition(',').first
+      when @user
+        user.location.partition(',').first
+    end  	end
+  end
 end
